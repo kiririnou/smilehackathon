867be924a7b7a9abb507bb0777f7984a8862f218
dvoyakiy <dvoyakiyyy@gmail.com>
2020-03-09 20:27:34 +0200

    move bootstrap import to App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,6 +5,7 @@ import MainPage from "./MainPage/MainPage";
 import UserInfoPage from "./UserInfoPage/UserInfoPage";
 
 import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function (props) {
     return (
