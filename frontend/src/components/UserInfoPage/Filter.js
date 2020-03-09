747aa1bf4b3822b15a7ca2b6231306307dd2af6e
dvoyakiy <dvoyakiyyy@gmail.com>
2020-03-09 20:28:30 +0200

    add time slider

--- frontend/src/components/UserInfoPage/Filter.js
+++ frontend/src/components/UserInfoPage/Filter.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-import'./css/Filter.css';
+import './css/Filter.css';
+import Range from "rc-slider/lib/Range";
+import 'rc-slider/assets/index.css';
 
 export default function(props){
+    function formatTime(){
+        const timeStamps = {};
+
+        for(let minutes = 0, idx = 0; minutes <= 1440; minutes+=60, idx++){
+            timeStamps[minutes] = `${Math.floor((minutes / 60))}:${(minutes % 60)}0`;
+        }
+
+        return timeStamps;
+    }
+
+
+
     return(
         <Card id={'filter'}>
             <Card.Body>
@@ -11,7 +27,24 @@ export default function(props){
                     Preferences
                 </Card.Title>
                 <Card.Text>
-
+                    <Form id={'filter-form'}>
+                        <Form.Group>
+                            <div id={'date-controls'}>
+                                <Form.Control type={'date'} id={'date-input'}/>
+                                <Form.Control type={'text'} id={'date-from'}/>
+                                <Form.Control type={'text'} id={'date-to'}/>
+                                <Button variant={'outline-primary'} id={'submit-date-btn'} type={'submit'}>Submit</Button>
+                            </div>
+                            <Range min={0}
+                                   max={1440}
+                                   marks={formatTime()}
+                                   defaultValue={[0, 1440]}
+                                   dots={true}
+                                   step={1}
+                                   pushable={1}
+                            />
+                        </Form.Group>
+                    </Form>
                 </Card.Text>
             </Card.Body>
         </Card>
