989842541f2725d3040b542aa93160c83f292e2e
dvoyakiy <dvoyakiyyy@gmail.com>
2020-03-16 19:09:01 +0200

    add dark theme

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Navbar} from 'react-bootstrap';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 
 import './css/Header.css'
 
@@ -9,6 +10,18 @@ export default function Header(props) {
             <Navbar.Brand href={'/'}>
                 <span>Tracker</span>
             </Navbar.Brand>
+            <div className="theme-picker">
+                <span style={{marginRight: '1rem', color: !props.darkTheme ? '#f8f9fa' : '#AFBDD1'}}>Theme</span>
+                <BootstrapSwitchButton checked={!props.darkTheme}
+                                       onstyle="light"
+                                       offstyle="dark"
+                                       onlabel={'light'}
+                                       offlabel={'dark'}
+                                       style={'theme-button'}
+                                       onChange={props.handleThemeSwitch}
+                />
+            </div>
+
         </Navbar>
     )
 }
